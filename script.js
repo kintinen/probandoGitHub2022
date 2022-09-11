18468973a096932815c8f24ecf8929cd149ebd71
Christian <christiankanemann@gmail.com>
2022-09-10 21:45:12 -0300

    Se le agrego un case para reemplazar el if

--- script.js
+++ script.js
@@ -18,16 +18,38 @@ function validar() {
 
     if (categoriaDescuento == 1) {
 
-        totalPagar = (valoTicket * cantidad) - (cantidad*valoTicket * 0.8)
+        totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.8)
 
     } else if (categoriaDescuento == 2) {
 
-        totalPagar = (valoTicket * cantidad) - (cantidad*valoTicket * 0.5)
+        totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.5)
 
     } else if (categoriaDescuento == 3) {
 
-        totalPagar = (valoTicket * cantidad) - (cantidad*valoTicket * 0.15)
+        totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.15)
     }
+    //  switch (categoriaDescuento) {
+
+    //    case (categoriaDescuento == 1):
+    //         var totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.8)
+    //          break
+
+    //      case (categoriaDescuento == 2):
+
+    //         var totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.5)
+
+    //         break
+
+    //     case (categoriaDescuento == 3):
+
+    //         var  totalPagar = (valoTicket * cantidad) - (cantidad * valoTicket * 0.15)
+
+    //          break
+
+    //      default:
+
+    //          break
+    //  }
 
     document.getElementById("totalaPagar").textContent = "Total a Pagar: $  " + totalPagar
 }
@@ -38,7 +60,7 @@ botonCalcular.addEventListener("click", validar)
 //document.getElementById("calcularValor").addEventListener("click", hacerClick)
 
 
-switch(categoriaDescuento){
+switch (categoriaDescuento) {
 
 
 
